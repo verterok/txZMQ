d954cf08bffb12bdaec641dbb5e3fa532746bbcf
Guillermo Gonzalez <guillo.gonzo@gmail.com>
2011-09-26 21:23:28 -0300

    Add timeout support to the ZmqXREQConnection

--- txZMQ/test/test_xreqxrep.py
+++ txZMQ/test/test_xreqxrep.py
@@ -2,7 +2,7 @@
 Tests for L{txZMQ.xreq_xrep}.
 """
 from twisted.trial import unittest
-from twisted.internet import defer
+from twisted.internet import defer, task, reactor, error, base
 
 from txZMQ.factory import ZmqFactory
 from txZMQ.connection import ZmqEndpointType, ZmqEndpoint
@@ -72,3 +72,130 @@ class ZmqConnectionTestCase(unittest.TestCase):
         """The request dict is cleanedup properly."""
         yield self.s.sendMsg('aaa')
         self.assertEqual(self.s._requests, {})
+
+
+class ZmqSlowTestXREPConnection(ZmqXREPConnection):
+
+    identity = 'service'
+    reactor = None
+    delay = 0.5
+
+    def gotMessage(self, message_id, *message_parts):
+        if not hasattr(self, 'messages'):
+            self.messages = []
+        self.messages.append([message_id, message_parts])
+        self.reactor.callLater(
+            self.delay, self.reply, message_id, *message_parts)
+
+
+class ZmqXREQConnectionTimeoutTestCase(unittest.TestCase):
+    """
+    Test case for timeouts in ZmqXREQConnection.
+    """
+
+    def setUp(self):
+        self.r_clock = task.Clock()
+        self.s_clock = task.Clock()
+        self.factory = ZmqFactory()
+        # set self.clock as the reactor in ZmqSlowTestXREPConnection
+        ZmqSlowTestXREPConnection.reactor = self.r_clock
+        self.r = ZmqSlowTestXREPConnection(self.factory,
+                      ZmqEndpoint(ZmqEndpointType.Bind, "ipc://test-sock"))
+        # set self.clock as the reactor in ZmqXREQConnection
+        ZmqXREQConnection.identity = 'client'
+        ZmqXREQConnection.reactor = self.s_clock
+        self.s = ZmqXREQConnection(self.factory,
+                      ZmqEndpoint(ZmqEndpointType.Connect, "ipc://test-sock"))
+        self.count = 0
+        def get_next_id():
+            self.count += 1
+            return 'msg_id_%d' % (self.count,)
+        self.s.get_next_id = get_next_id
+
+    def tearDown(self):
+        ZmqXREQConnection.identity = None
+        self.factory.shutdown()
+
+    def iterate(self, delay=0.01):
+        """Allow the reactor to spin a bit."""
+        return task.deferLater(reactor, delay, lambda: defer.succeed(None))
+
+    @defer.inlineCallbacks
+    def test_timeout_set(self):
+        """Test that timeout is set."""
+        now = self.r_clock.seconds()
+        d = self.s.sendMsg('aaa', timeout=5)
+        request = self.s._requests.values()[0]
+        self.assertIsInstance(request, defer.Deferred)
+        timeout = self.s._timeouts.values()[0]
+        self.assertIsInstance(timeout, base.DelayedCall)
+        self.assertEqual(timeout.time, 5)
+        yield self.iterate()
+        self.r_clock.advance(1)
+        yield d
+
+    @defer.inlineCallbacks
+    def test_timeout_called(self):
+        """Test that timeout is called."""
+        d = self.s.sendMsg('aaa', timeout=2)
+        self.assertFalse(d.called)
+        self.s_clock.advance(3)
+        self.assertTrue(d.called)
+        try:
+            yield d
+        except error.TimeoutError:
+            self.assertEqual({}, self.s._timeouts)
+        else:
+            self.fail("Should fail with TimeoutError.")
+
+    @defer.inlineCallbacks
+    def test_timeout_called_and_reply(self):
+        """Test that timeout is called, but we also get a reply."""
+        # patch messageReceived in order to know if it's called
+        called = []
+        msg_recv = self.s.messageReceived
+        def intercept(msg):
+            """Collect calls."""
+            msg_recv(msg)
+            called.append(msg)
+        self.patch(self.s, 'messageReceived', intercept)
+        d = self.s.sendMsg('aaa', timeout=2)
+        self.assertFalse(d.called)
+        self.s_clock.advance(3)
+        yield self.iterate()
+        self.assertTrue(d.called)
+        self.r_clock.advance(1)
+        yield self.iterate()
+        # check that messageReceived was called
+        self.assertEqual(called[0][2:], ['aaa'])
+        try:
+            yield d
+        except error.TimeoutError:
+            self.assertEqual({}, self.s._timeouts)
+        else:
+            self.fail("Should fail with TimeoutError.")
+
+    @defer.inlineCallbacks
+    def test_lots_of_timeouts(self):
+        """Test that it also works for more than one timeout."""
+        now = self.r_clock.seconds()
+        deferreds = []
+        times = {}
+        for i in range(100):
+            self.s_clock.advance(0.1)
+            d = self.s.sendMsg('aaa_%d' % (i,), timeout=100)
+            times[d] = self.s_clock.seconds()
+            deferreds.append(d)
+        self.assertEqual(100, len(self.s._requests))
+        for msg_id, request in self.s._requests.items():
+            self.assertIsInstance(request, defer.Deferred)
+            timeout = self.s._timeouts[msg_id]
+            self.assertIsInstance(timeout, base.DelayedCall)
+            self.assertEqual(timeout.time, times[request]+100)
+
+        yield self.iterate()
+        self.r_clock.advance(0.1*101)
+        yield defer.DeferredList(deferreds)
+        self.assertEqual({}, self.s._timeouts)
+        self.assertEqual({}, self.s._requests)
+
--- txZMQ/xreq_xrep.py
+++ txZMQ/xreq_xrep.py
@@ -2,7 +2,7 @@
 ZeroMQ PUB-SUB wrappers.
 """
 
-from twisted.internet import defer
+from twisted.internet import defer, error
 from zmq.core import constants
 
 from txZMQ.connection import ZmqConnection
@@ -13,24 +13,53 @@ class ZmqXREQConnection(ZmqConnection):
     A XREQ connection.
     """
     socketType = constants.XREQ
+    timeout = 30
+    reactor = None
 
     def __init__(self, factory, *endpoints):
         ZmqConnection.__init__(self, factory, *endpoints)
+        # set the default reactor if there none
+        if self.reactor is None:
+            from twisted.internet import reactor
+            self.reactor = reactor
         self._requests = {}
+        self._timeouts = {}
+
+    def shutdown(self):
+        """Override shutdown to stop the timeout loop."""
+        # cancel all timeouts
+        for msg_id in self._requests:
+            timeout = self._timeouts[msg_id]
+            if timeout.active:
+                timeout.cancel()
+        return ZmqConnection.shutdown(self)
 
     def get_next_id(self):
         """Returns an unique id."""
         raise NotImplementedError(self)
 
-    def sendMsg(self, *message_parts):
+    def _requestTimeout(self, msg_id):
+        """Timeout a specific request."""
+        # remove the timeout from the _timeouts dict
+        self._timeouts.pop(msg_id, None)
+        # remove the deferred from the _requests dict and call the errback
+        d = self._requests.pop(msg_id, None)
+        if d:
+            d.errback(error.TimeoutError(string='Request timeout.'))
+
+    def sendMsg(self, *message_parts, **kwargs):
         """
         Send L{message} with specified L{tag}.
 
         @param message_parts: message data
         @type message: C{tuple}
         """
+        timeout = kwargs.pop('timeout', self.timeout)
         d = defer.Deferred()
         message_id = self.get_next_id()
+        timeoutCall = self.reactor.callLater(timeout,
+                                             self._requestTimeout, message_id)
+        self._timeouts[message_id] = timeoutCall
         self._requests[message_id] = d
         message = [message_id, '']
         message.extend(message_parts)
@@ -44,8 +73,15 @@ class ZmqXREQConnection(ZmqConnection):
         @param message: message data
         """
         msg_id, _, msg = message[0], message[1], message[2:]
-        d = self._requests.pop(msg_id)
-        d.callback(msg)
+        timeout = self._timeouts.pop(msg_id, None)
+        # remove the timeout from the _timeouts dict and cancel it
+        if timeout and timeout.active:
+            timeout.cancel()
+        # only callback if the request is still there, it might have been
+        # removed by timeout
+        d = self._requests.pop(msg_id, None)
+        if d:
+            d.callback(msg)
 
 
 class ZmqXREPConnection(ZmqConnection):
